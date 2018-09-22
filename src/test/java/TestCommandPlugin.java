53812b561ed32ca3f907cef902c42db633936e40
adriens <adrien.sales@gmail.com>
2018-09-22 14:06:22 +1100

    Initial load

---
+++ src/test/java/TestCommandPlugin.java
@@ -0,0 +1,19 @@
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import schemacrawler.tools.executable.CommandRegistry;
+
+public class TestCommandPlugin
+{
+
+  @Test
+  public void testCommandPlugin()
+    throws Exception
+  {
+    final CommandRegistry registry = new CommandRegistry();
+    assertTrue(registry.getSupportedCommands().contains("csv"));
+  }
+
+}
