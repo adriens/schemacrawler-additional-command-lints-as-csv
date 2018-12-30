2a4b7268c5e7c175149eb4f11e1047e46c2c204b
Sualeh Fatehi <sualeh@hotmail.com>
2018-12-29 19:24:24 -0500

    Get Oracle dbCatalogName - see #9

--- src/main/java/com/github/adriens/AdditionalCommand.java
+++ src/main/java/com/github/adriens/AdditionalCommand.java
@@ -228,8 +228,8 @@ public class AdditionalCommand
 
     if (serverInfoMap.containsKey("SERVICE_NAME"))
     {
-      // Oracle SERVICE_NAME
-      dbCatalogName = serverInfoMap.get("SERVICE_NAME");
+      // Oracle GLOBAL_NAME
+      dbCatalogName = serverInfoMap.get("GLOBAL_NAME");
     }
     else if (serverInfoMap.containsKey("current_database"))
     {
