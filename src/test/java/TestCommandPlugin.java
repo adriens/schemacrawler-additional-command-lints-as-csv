35b9220d13b04ed4eff6908336df11061d27b037
Michèle BARRE <michele.barre@opt.nc>
2020-09-15 14:10:06 +1100

    fix \#60 Upgrade to latest schemacrawler runtime > 15.04.01 ->16.9.4

--- src/test/java/TestCommandPlugin.java
+++ src/test/java/TestCommandPlugin.java
@@ -1,19 +1,33 @@
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.github.adriens.AdditionalCommand;
 import org.junit.Test;
 
+import schemacrawler.tools.executable.CommandDescription;
 import schemacrawler.tools.executable.CommandRegistry;
+
+import java.util.Iterator;
 
 public class TestCommandPlugin
 {
 
   @Test
-  public void testCommandPlugin()
-    throws Exception
+  public void testCommandPlugin() throws Exception
   {
-    final CommandRegistry registry = new CommandRegistry();
-    assertTrue(registry.isCommandSupported("csv"));
+    final CommandRegistry registry = CommandRegistry.getCommandRegistry();
+    assertTrue(registry.getSupportedCommands().size() > 0);
+    Iterator<CommandDescription> it = registry.getSupportedCommands().iterator();
+    boolean csvCommandFound = false;
+    while (it.hasNext()){
+      if(it.next().getName().equals("csv")){
+        csvCommandFound = true;
+        break;
+      }
+    }
+
+    assertTrue(csvCommandFound);
   }
 
 }
